4cd4bebed73f237b402e981e7de8d37596e9ab63
KongRain <kongxygaoykk@163.com>
2015-07-13 22:20:06 +0800

    fileSystem
    文件处理练习

---
+++ fileSystem/copy2.js
@@ -0,0 +1,14 @@
+/**
+ * Created by xyk on 2015/7/13.
+ */
+var fs = require('fs');
+
+function copy(src, dst) {
+    fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+}
+
+function main(argv) {
+    copy(argv[0], argv[1]);
+}
+
+main(process.argv.slice(2));
