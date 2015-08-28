beb76190fa555279789dc75de2228a11bae3a64f
KongRain <kongxygaoykk@163.com>
2015-08-28 10:29:21 +0800

    整理文档，添加读书笔记

--- school/student.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by xyk on 2015/7/3.
- */
-function add(student) {
-    console.log("Add Student: " + student);
-}
-
-exports.add = add;
--- school/teacher.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by xyk on 2015/7/3.
- */
-function add(teacher) {
-    console.log("Add Teacher: " + teacher);
-}
-
-exports.add = add;
